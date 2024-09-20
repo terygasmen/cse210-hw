da84ce3df5642347c584d8bb50c82d2c171677c7
terygasmen <terygasmen@gmail.com>
2024-09-20 19:35:15 +0800

    finished prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // first name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        // last name
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+        // result
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
