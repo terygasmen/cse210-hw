dcf94b1ba1da642fe9ed458ccf2f221a9b1e809a
terygasmen <terygasmen@gmail.com>
2024-09-20 19:57:33 +0800

    core prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // generate number bet 1 and 100
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != magicNumber) {
+            // ask for a guess
+            Console.Write("What is your guess? ");
+            string guessInput = Console.ReadLine();
+            guess = int.Parse(guessInput);
+            // check if higher, lower or correct
+            if (guess < magicNumber) {
+                Console.WriteLine("Higher");
+            } else if (guess > magicNumber) {
+                Console.WriteLine("Lower");
+            } else {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
