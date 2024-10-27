d3eb8c849b6d8f7a0f1357c3b8dae48d20690f64
terygasmen <terygasmen@gmail.com>
2024-10-27 09:22:40 +0800

    shapes prep w06

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Collections.Generic;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning06 World!");
-    }
+class Program{
+        static void Main(string[] args) {
+                Shape square = new Square("Red", 5);
+                Shape rectangle = new Rectangle("Blue", 4, 7);
+                Shape circle = new Circle("Green", 3);
+
+                List<Shape> shapes = new List<Shape> { square, rectangle, circle};
+
+                foreach (Shape shape in shapes) {
+                        Console.WriteLine($"Color: {shape.GetColor()}, Area: {shape.GetArea():F2}");
+                }
+        }
 }
